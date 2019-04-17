8e62ddd623bb18ff5c5f36ff715489e657cbc22d
Ivan Chowdhury <moaggaimc@gmail.com>
2019-04-17 17:38:22 -0400

    .

--- match-frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './Root';
-import './index.css';
-import configureStore from 'redux/configureStore';
-
-const store = configureStore();
-ReactDOM.render(<Root store={store}/>, document.getElementById('root'));
