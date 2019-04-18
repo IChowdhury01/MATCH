1595df7ee11c5855c01eb07eeaa861b3d30ba822
hgoldfarb <46955484+hgoldfarb@users.noreply.github.com>
2019-04-17 20:23:29 -0400

    more sparky stuff

--- spark/src/main/java/App.java
+++ spark/src/main/java/App.java
@@ -15,8 +15,22 @@ public class App {
         post("/login",(req,res)-> {
             String name = req.queryParams("username");
             String pass = req.queryParams("password");
-            return "Your username is " + name + " and your password is " + pass + ".";
+            if (name.equals("Ivan") && pass.equals("123van"))
+                return "Correct! Your username is " + name + " and your password is " + pass + ".";
+            else
+                return "You entered the username " + name + " and the password " + pass + ". It's wrong. Try \"Ivan\" and \"123van\".";
+
         });
+
+        post("/register",(req,res)-> {
+            String name = req.queryParams("username");
+            String pass = req.queryParams("password");
+            String displayName = req.queryParams("displayName");
+            String maxTravelDistance = req.queryParams("maxTravelDistance");
+            String aboutMe = req.queryParams("aboutMe");
+            return "Registration successful!";
+        });
+
 
 
     }
