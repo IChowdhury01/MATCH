c1570bcc85e5bc54d4f0bd8f6125266efd8aa076
hgoldfarb <46955484+hgoldfarb@users.noreply.github.com>
2019-04-22 02:44:03 -0400

    added function to calculate distance

--- spark/src/main/java/com/MatchApp.java
+++ spark/src/main/java/com/MatchApp.java
@@ -105,4 +105,21 @@ public class MatchApp {
 //        MatchJDBC.userLogin(username,password);
 //        MatchJDBC.getUser(usrname);
     }
+    
+    public static double distance(double lat1, double lat2, double long1, double long2) {
+        //https://stackoverflow.com/questions/3694380/calculating-distance-between-two-points-using-latitude-longitude
+
+        final int R = 6371; // Radius of the earth
+
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(long2 - long1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c * 1000; // convert to meters
+
+        return Math.sqrt(distance);
+    }
+
 }
